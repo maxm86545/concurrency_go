97e9038cce70e34a3f0d1a88d311d9a785d39d8f
Max <49632507+maxm86545@users.noreply.github.com>
2025-09-17 11:25:31 +0300

    logger.MakeFileLogger

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,14 +8,13 @@ import (
 	"syscall"
 
 	"go.uber.org/multierr"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 	"golang.org/x/sync/errgroup"
 
 	"github.com/maxm86545/concurrency_go/internal/cli"
 	"github.com/maxm86545/concurrency_go/internal/database"
 	"github.com/maxm86545/concurrency_go/internal/database/compute"
 	"github.com/maxm86545/concurrency_go/internal/database/storage"
+	"github.com/maxm86545/concurrency_go/internal/logger"
 )
 
 const maxCommandLen = 128
@@ -30,14 +29,14 @@ func run() (errReturned error) {
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
-	logger, err := makeLogger("app.log")
+	log, err := logger.MakeFileLogger("app.log")
 	if err != nil {
 		return fmt.Errorf("create logger: %w", err)
 	}
-	defer multierr.AppendFunc(&errReturned, logger.Sync)
+	defer multierr.AppendFunc(&errReturned, log.Sync)
 
 	db := database.NewDatabase(
-		logger,
+		log,
 		compute.NewCompute(maxCommandLen),
 		storage.NewStorage(),
 	)
@@ -64,26 +63,4 @@ func run() (errReturned error) {
 	})
 
 	return eg.Wait()
-}
-
-func makeLogger(fileName string) (*zap.Logger, error) {
-	f, err := os.OpenFile(fileName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0o644)
-	if err != nil {
-		return nil, fmt.Errorf("open log file: %w", err)
-	}
-
-	cfg := zap.NewProductionConfig()
-	cfg.OutputPaths = []string{}
-	cfg.DisableStacktrace = true
-	cfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
-
-	return cfg.Build(zap.WrapCore(func(core zapcore.Core) zapcore.Core {
-		fileCore := zapcore.NewCore(
-			zapcore.NewJSONEncoder(cfg.EncoderConfig),
-			zapcore.AddSync(f),
-			cfg.Level,
-		)
-
-		return zapcore.NewTee(core, fileCore)
-	}))
 }
